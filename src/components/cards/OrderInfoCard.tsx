c2ea1797551eb3261b62905c5be51a5e01fcfd6e
jeriatno <jeriat01@gmail.com>
2025-04-30 08:43:35 +0800

    feat: change balance color based on positive/negative value

--- src/components/cards/OrderInfoCard.tsx
+++ src/components/cards/OrderInfoCard.tsx
@@ -16,6 +16,7 @@ type Props = {
 };
 
 export default function OrderInfoCard({ data }: Props) {
+    // @ts-ignore
     return (
         <Card className="bg-white rounded-xl shadow-xl border-none">
             <CardContent>
@@ -44,7 +45,13 @@ export default function OrderInfoCard({ data }: Props) {
                         </div>
                         <div>
                             <p className="text-base text-gray-500">Balance</p>
-                            <p className="text-xl font-bold text-red-600 mt-1">{data?.balance || 0}</p>
+                            <p
+                                className={`text-xl font-bold mt-1 ${
+                                    data!.balance >= 0 ? 'text-green-600' : 'text-red-600'
+                                }`}
+                            >
+                                {data!.balance}
+                            </p>
                             <p className="text-sm text-gray-500">Remaining target</p>
                         </div>
                         <div>
