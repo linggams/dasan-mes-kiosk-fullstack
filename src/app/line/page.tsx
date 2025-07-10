ad2d3d1210c16b98792149c641dc924c10d118a0
R. Agus Iman Sudrajat <agus.sudrajat@os-selnajaya.com>
2025-07-10 15:25:21 +0700

    fix: refactor LinePage component and replace DummyData with LineDataCard for improved data display with integrate API

--- src/app/line/page.tsx
+++ src/app/line/page.tsx
@@ -7,18 +7,16 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { useRequestDetail } from "@/hooks/useRequestDetail";
-import { useUpdateStage } from "@/hooks/useUpdateStage";
-import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
-import DummyData from "@/components/cards/DummyData";
+import LineDataCard from "@/components/cards/LineDataCard";
+import { useRequestLines } from "@/hooks/useRequestLines";
 
 export default function LinePage() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
-  const [selectedQrCode, setSelectedQrCode] = useState<string | null>(null);
-  const [open, setOpen] = useState(false);
-  const [date, setDate] = useState<Date | undefined>(undefined);
+  const baseUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1`;
+
+  const { isLoading, selectedRequestLines } = useRequestLines(baseUrl);
 
   const [selectedDate, setSelectedDate] = useState(() => {
     const today = new Date();
@@ -28,50 +26,8 @@ export default function LinePage() {
   const handleDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const newDate = e.target.value;
     setSelectedDate(newDate);
-    // if (onDateChange) {
-    //     onDateChange(newDate);
-    // }
-  };
-  const baseUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1`;
-
-  const [line, setLine] = useState("1");
-
-  const { selectedRequestId, selectedRequest, fetchRequestDetail } =
-    useRequestDetail(baseUrl, line);
-
-  const { updateStage } = useUpdateStage({
-    baseUrl,
-    line,
-    selectedQrCode,
-    selectedRequestId,
-  });
-
-  const handleOpenRequestModal = () => {
-    // setFormData(defaultFormData);
-    // setIsRequestModalOpen(true);
   };
 
-  const isToday = (dateStr: string) => {
-    const requestDate = new Date(dateStr);
-    const today = new Date();
-
-    return (
-      requestDate.getFullYear() === today.getFullYear() &&
-      requestDate.getMonth() === today.getMonth() &&
-      requestDate.getDate() === today.getDate()
-    );
-  };
-
-  const dummy = {
-    actual: [
-      10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160,
-      170, 180, 190, 200, 210, 220, 230, 240,
-    ],
-    cumulative: [
-      10, 30, 60, 100, 150, 210, 280, 360, 450, 550, 660, 780, 910, 1050, 1200,
-      1360, 1530, 1710, 1900, 2100, 2310, 2530, 2760, 3000,
-    ],
-  };
   return (
     <div className="min-h-screen bg-gray-100">
       <nav className="flex items-center justify-between w-full p-4 bg-white shadow">
@@ -97,10 +53,7 @@ export default function LinePage() {
             variant="outline"
             // onClick={() => setIsSidebarOpen(!isSidebarOpen)}
           >
-            <FontAwesomeIcon
-              icon={isSidebarOpen ? faTimes : faBars}
-              className="text-gray-500"
-            />
+            <FontAwesomeIcon icon={faTimes} className="text-gray-500" />
             Refresh
           </Button>
         </div>
@@ -108,9 +61,19 @@ export default function LinePage() {
 
       <section className="mx-auto mt-4 xl:max-w-[100rem] max-w-4xl">
         <div className="grid grid-cols-1 gap-8">
-          {[1, 2, 3].map((line) => (
-            <DummyData key={line} data={dummy} />
-          ))}
+          {isLoading ? (
+            <div className="flex items-center justify-center h-64">
+              <p className="text-gray-500">Loading...</p>
+            </div>
+          ) : selectedRequestLines.length > 0 ? (
+            selectedRequestLines.map((lines, idx: number) => (
+              <LineDataCard key={lines.line_info.line + idx} data={lines} />
+            ))
+          ) : (
+            <p className="text-center text-gray-500">
+              No data available for the selected date.
+            </p>
+          )}
         </div>
       </section>
     </div>
--- src/types/request.ts
+++ src/types/request.ts
@@ -1,26 +1,35 @@
 export type Request = {
-    id: number;
-    code: string;
-    status: string;
-    buyer: string;
-    style: string;
-    date: string;
+  id: number;
+  code: string;
+  status: string;
+  buyer: string;
+  style: string;
+  date: string;
 };
 
 export type RequestFormData = {
-    buyer_id?: number;
-    cutting_id?: number;
-    line_id?: number;
-    supervisor_id?: number;
+  buyer_id?: number;
+  cutting_id?: number;
+  line_id?: number;
+  supervisor_id?: number;
 };
 
 export type RequestData = {
-    code: string;
-    buyer: string;
-    style: string;
+  code: string;
+  buyer: string;
+  style: string;
 };
 
 export type ProductionData = {
-    actual: number[];
-    cumulative: number[];
+  actual: number[];
+  cumulative: number[];
+};
+
+export type LineInfoTypes = {
+  factory: string;
+  line: string;
+  request_id: string;
+  buyer: string;
+  style: string;
+  status: string;
 };
