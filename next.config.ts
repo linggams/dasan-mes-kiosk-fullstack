a09a638b44cdd93c42639c0070c9daadf72dc8de
jeriatno <jeriat01@gmail.com>
2025-08-28 21:55:00 +0800

    change config

--- next.config.ts
+++ next.config.ts
@@ -1,37 +1,31 @@
 import type { NextConfig } from "next";
 
-const protocol =
-  process.env.NEXT_PUBLIC_API_PROTOCOL === "https" ? "https" : "http";
-const hostname =
-  process.env.NEXT_PUBLIC_API_BASE_URL?.replace("https://", "").replace(
-    "http://",
-    ""
-  ) ?? "";
-const destination = `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/:path*`;
+const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://127.0.0.1:8000";
+const url = new URL(apiUrl);
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: protocol,
-        hostname: hostname,
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "dppicms.devta.id",
-      },
-    ],
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: destination,
-      },
-    ];
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: url.protocol.replace(":", ""), // "http" / "https"
+                hostname: url.hostname, // "127.0.0.1"
+                port: url.port || "", // "8000"
+                pathname: "/**",
+            },
+            {
+                protocol: "https",
+                hostname: "dppicms.devta.id",
+            },
+        ],
+    },
+    async rewrites() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: `${apiUrl}/api/:path*`,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
